a97a7c2a8f2134a405e74c4fb4d1fdaebbb92eb6
Rafael Carvalho <rafaeloc.dev@gmail.com>
2021-06-01 16:53:10 -0300

    Updade Html, CSS e Javascrip
    Feito estrutura do Html, estilização básica em CSS e lógica de adição de produtos a lista de compras em Js finalizada.

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,1 +1,27 @@
-﻿
+﻿let nameItem = document.getElementById('name-item');
+let btnAddItem = document.getElementById('btn-add-item');
+let ul = document.getElementById('list-items')
+
+function addElement() {
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+
+    deleteButton.innerHTML = "X";
+    deleteButton.className = "delete";
+    
+    li.appendChild(deleteButton);
+
+    let descriptionItem = document.createTextNode(nameItem.value);
+
+    li.appendChild(descriptionItem);
+    ul.appendChild(li)
+
+    nameItem.value = ''
+}
+
+function deleteProduto() {
+    
+}
+
+btnAddItem.addEventListener('click', addElement);
+deleteButton.addEventListener('click', deleteProduto);
