0c68da8e535ee23f75d7e9f227820e36e0183eda
Rafael Carvalho <rafaeloc.dev@gmail.com>
2021-06-03 14:12:18 -0300

    Finalizado logica do botao de riscar items, algumas melhorias no CSS e adiçao de imagens na pasta do projeto para futura melhoria no botao de riscar items

--- Projeto/script.js
+++ Projeto/script.js
@@ -2,27 +2,46 @@
 let btnAddItem = document.getElementById('btn-add-item');
 let ul = document.getElementById('list-items');
 let listItemsDelete = document.getElementsByClassName('delete');
+let listButtonCheck = document.getElementsByClassName('check')
 
 function addElement() {
     let li = document.createElement('li');
+    let checkButton = document.createElement('button')
     let deleteButton = document.createElement('button');
+    let descriptionItem = document.createTextNode(nameItem.value);
+
+    descriptionItem.style = "text: upper"
+
+    checkButton.innerHTML = "OK"
+    checkButton.className = "check"
 
     deleteButton.innerHTML = "X";
     deleteButton.className = "delete";
     
+    li.appendChild(checkButton);
     li.appendChild(deleteButton);
 
-    let descriptionItem = document.createTextNode(nameItem.value);
 
     li.appendChild(descriptionItem);
     ul.appendChild(li)
 
     nameItem.value = ''
     addFunctionDeleteButton();
+    addFunctionCheckButton();
 }
 
 function deleteProduto() {
     this.parentElement.remove();
+}
+
+function checkProduto() {
+    this.parentElement.style = "text-decoration: line-through;" + "color: #F0E68C;"
+}
+
+function addFunctionCheckButton() {
+    for(let i = 0; i < listButtonCheck.length; i++ ) {
+        listButtonCheck[i].addEventListener('click', checkProduto)
+    }
 }
 
 function addFunctionDeleteButton() {
@@ -33,3 +52,4 @@ function addFunctionDeleteButton() {
 
 btnAddItem.addEventListener('click', addElement);
 addFunctionDeleteButton();
+addFunctionCheckButton();
