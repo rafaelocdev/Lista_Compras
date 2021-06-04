c870d17ca1399d48b9561a5506c0cf2676e21663
Rafael Carvalho <rafaeloc.dev@gmail.com>
2021-06-04 13:28:03 -0300

    Updates diversos
    1 - Finalizada função de ticar itens, incluindo essa função ao texto do item também e trocando a imagem do botão de ticar.
    
    2 - Realocado o botão de delete e modificada a imagem do botão.

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,31 +1,37 @@
 ﻿let nameItem = document.getElementById('input-name-item');
 let btnAddItem = document.getElementById('btn-add-item');
 let ul = document.getElementById('list-items');
-let listItemsDelete = document.getElementsByClassName('delete');
-let listButtonCheck = document.getElementsByClassName('check')
+let listItemsDelete = document.getElementsByClassName('btn-delete');
+let listButtonCheck = document.getElementsByClassName('btn-check');
+let listNameCheck = document.getElementsByClassName('name-item');
 
 function addElement() {
     let li = document.createElement('li');
     let checkButton = document.createElement('img')
-    let deleteButton = document.createElement('button');
+    let deleteButton = document.createElement('img');
     let p = document.createElement('p');
     let descriptionItem = document.createTextNode(nameItem.value);
+    let divCheckName = document.createElement('div');
 
     descriptionItem.style = "text: upper";
 
     checkButton.src = "/img/no_ticado.png";
     checkButton.alt = "não-ticado";
-    checkButton.className = "check";
+    checkButton.className = "btn-check";
 
-    deleteButton.innerHTML = "X";
-    deleteButton.className = "delete";
+    deleteButton.src = "/img/trash.png";
+    deleteButton.alt = "Deletar";
+    deleteButton.className = "btn-delete";
 
-    p.className = "name-item"
+    divCheckName.style.display = "flex"
+
+    p.className = "name-item";
     
     p.append(descriptionItem);
-    li.appendChild(checkButton);
+    divCheckName.appendChild(checkButton);
+    divCheckName.appendChild(p);
+    li.appendChild(divCheckName);
     li.appendChild(deleteButton);
-    li.appendChild(p);
 
     ul.appendChild(li)
 
@@ -41,15 +47,16 @@ function deleteProduto() {
 }
 
 function checkProduto() {
-    this.parentElement.children[2].style = "text-decoration: line-through;" + "color: #F0E68C;"
-    this.checkButton.src = "/img/ticado.png"
-    this.checkButton.alt = "ticado"
+    this.parentElement.children[1].style = "text-decoration: line-through;" + "color: #F0E68C;"
+    this.parentElement.children[0].src = "/img/ticado.png"
+    this.parentElement.children[0].alt = "ticado"
     nameItem.focus();
 }
 
 function addFunctionCheckButton() {
     for(let i = 0; i < listButtonCheck.length; i++ ) {
         listButtonCheck[i].addEventListener('click', checkProduto)
+        listNameCheck[i].addEventListener('click', checkProduto)
     }
 }
 
